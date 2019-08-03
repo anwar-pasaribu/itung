a1336ef1918fb80a16326c1444400838e33a8225
anwarpasaribu <anwar_pasaribu@live.com>
2019-08-03 15:28:59 +0700

    Begin learn play ground

---
+++ app/src/main/java/com/unware/itung/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.unware.itung
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.unware.itung.ui.extension.setupWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    private var currentNavController: LiveData<NavController>? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        }
+    }
+
+    /**
+     * Called on first creation and when restoring state.
+     */
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+
+        val navGraphIds = listOf(
+            R.navigation.navigation_home,
+            R.navigation.navigation_dashboard,
+            R.navigation.navigation_notifications
+        )
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_fragment,
+            intent = intent
+        )
+
+        // Whenever the selected controller changes, setup the action bar.
+        controller.observe(this, Observer { navController ->
+            setupActionBarWithNavController(navController)
+        })
+        currentNavController = controller
+    }
+}
